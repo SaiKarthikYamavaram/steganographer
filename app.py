e88f81e4d0e9c7362ee63fca2edab7686dfd1244
sai karthik yamavaram <ysaikarthiksep01@gmail.com>
2020-08-15 18:09:18 +0530

    added file download

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, send_file
 from flask import *
 import os
 
 from cryptography.fernet import Fernet
 import helper
 from helper.steganography import *
-app = Flask(__name__, instance_path='D:\\mini-project\\flask\\upload\\')
-
+app = Flask(__name__, instance_path='D:\\mini-project\\flask\\steganographer\\decryption_input')
+app.config['CLIENT_IMG']= "D:\mini-project\\flask\steganographer\encrypted_output"
 
 def TxtDecrypt(data, key):
-    data = bytes(data, 'utf-8')
-    print(data)
+    # print(data)
+    # data = str.decode(data)
     f = Fernet(key)
     decrypted = f.decrypt(data)
     return decrypted
@@ -20,13 +20,14 @@ def TxtEncrypt(data, key):
     data_encoded = data.encode()  # Converting message string to utf-8 format
     fObject = Fernet(key)  # a Fernet object
     data1 = fObject.encrypt(data_encoded)
-    data1 = TxtDecrypt(data1, key)
     return data1
+
 
 @app.route('/get-img')
 def get_image():
     photo = "upload\download.png"
-    return send_file(photo,mimetype="image/png")
+    return send_file(photo, mimetype="image/png")
+
 
 @app.route('/')
 def index():
@@ -49,9 +50,12 @@ def on_upload():
         file = open('key.key', 'rb')  # Open the file as wb to read bytes
         key = file.read()  # The key will be type bytes
         file.close()
+        print(request.form['encryption-data'],
+              type(request.form['encryption-data']))
         decrypted_text = TxtEncrypt(request.form['encryption-data'], key)
-        hide_data_to_image(str.encode(decrypted_text))
-        return decrypted_text
+        hide_data_to_image(decrypted_text)
+        return send_from_directory(app.config["CLIENT_IMG"], filename="_with_hidden_file.png", as_attachment=True)
+
 
     else:
         return render_template('error.html')
@@ -64,8 +68,11 @@ def on_upload_decryption():
         key = file.read()  # The key will be type bytes
         file.close()
         f = request.files["decryption-file"]
-        # f.save(os.path.join(app.instance_path, f.filename))
-        decrypted_text = TxtDecrypt(request.form['decryption-data'], key)
+        img_loc = os.path.join(app.instance_path, f.filename)
+        f.save(img_loc)
+        encrypted_text = extract_message_from_image(img_loc)
+        # print(type(request.form['decryption-data']),type(encrypted_text))
+        decrypted_text = TxtDecrypt(encrypted_text, key)
         return decrypted_text
 
     else:
--- helper/steganography.py
+++ helper/steganography.py
@@ -8,13 +8,7 @@ def __change_last_2_bits__(image_pixel_values, data_bits):
     return image_pixel_values >> 2 << 2 | data_bits
 
 
-def hello():
-    image = Image.open( "D:\\mini-project\\flask\\steganographer\\static\\images\\encrypt\\download.png")
-    # print(filecmp.cmp(image, img))
-    print(image)
-
-
-def hide_data_to_image( data_to_hide):
+def hide_data_to_image(data_to_hide):
     """
     This function hides the data_to_hide inside the image located at input_file_path,
     and saves this modified image to output_file_path.
@@ -34,7 +28,8 @@ def hide_data_to_image( data_to_hide):
 
     """
     input_file_path = "D:\\mini-project\\flask\\steganographer\\static\\images\\encrypt\\download.png"
-    output_file_path =  "D:\\mini-project\\flask\\steganographer\\output\\"+ "_with_hidden_file" + "." + input_file_path.split(".")[-1]
+    output_file_path = "D:\\mini-project\\flask\\steganographer\\encrypted_output\\" + \
+        "_with_hidden_file" + "." + input_file_path.split(".")[-1]
     image = Image.open(input_file_path).convert('RGB')
     pixels = image.load()
 
@@ -107,8 +102,68 @@ def hide_data_to_image( data_to_hide):
     image.save(output_file_path)
 
 
-data = "hello world"
-data = str.encode(data)
-hide_data_to_image(data)
+def extract_message_from_image(input_file_path):
 
-# hello()
+    image = Image.open(input_file_path).convert('RGB')
+    pixels = image.load()
+
+    # List where we will store the extracted bits
+    data_stack = list()
+    end_of_msg = False
+
+    for image_y_index in range(image.size[1]):
+        for image_x_index in range(image.size[0]):
+            # Read pixel values traversing from [0, 0] to the end
+            pixel = pixels[image_x_index, image_y_index]
+
+            # Extract hidden message in chunk of 2 bits from each Channel
+            data_stack.append(pixel[0] & 0b11)
+            data_stack.append(pixel[1] & 0b11)
+            data_stack.append(pixel[2] & 0b11)
+
+            if len(data_stack) >= 16 and set(data_stack[-16:]) == set(b"\x00"):
+                # We encountered 4 '\0' characters, that means our hidden message ends here
+                end_of_msg = True
+                break
+
+        if end_of_msg:
+            break
+
+    if not end_of_msg:
+        # That means we didn't encountered any '\0' character
+        print("WARNING : Incomplete Hidden Message")
+        print("WARNING : Image may be corrupted or no Message was hidden in this Image")
+
+    data = bytes()
+
+    for i in range(0, len(data_stack) - 4, 4):
+        # Extract 2 bits 4 times i.e 8 bits ( We will get one UTF-8 Character)
+        chunk = data_stack[i:i + 4]
+
+        # The integer value of byte we will extract from the image
+        recovered_int = 0
+
+        # Extracting 8th and 7th bit respectively
+        recovered_int |= chunk[0] << 6
+        # Extracting 6th and 5th bit respectively
+        recovered_int |= chunk[1] << 4
+        # Extracting 4th and 3rd bit respectively
+        recovered_int |= chunk[2] << 2
+        # Extracting 2nd and 1st bit respectively
+        recovered_int |= chunk[3]
+
+        if len(data) >= 3 and recovered_int == 0 and set(data[-3:]) == set(b"\x00"):
+            # 4 '\0' characters found, end of hidden file
+            data += bytes([recovered_int])
+            break
+        else:
+            # Putting extracted byte (8 bits) to the right place
+            data += bytes([recovered_int])
+
+    if end_of_msg:                                      # If the hidden file is complete
+        # Remove last 4 '\0' characters
+        data = data[:-4]
+
+    # print(f"* INFO : Saving hidden file to {output_file_path}")
+    print(f"* INFO : Size of hidden file recovered : {len(data)} bytes")
+    return data
